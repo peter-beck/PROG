0396344c1cfc1ef514b37670e7b6216d64b8bc0e
Peter <peterbeck096@gmail.com>
2018-03-28 01:19:31 +0200

    Add files via upload

--- zadanie4.sh
+++ zadanie4.sh
@@ -13,43 +13,49 @@ fi
 
 #read IP #input
 
-echo -e "NAJDENE IP:\n" > IP.txt   #clear the IP.txt file with initial message
-echo -e "VYSTUP PING:\n" > out.txt  #clear the out.txt file with initial message
+echo -e "USER OUTPUT:" > uOUT.txt   #clear the IP.txt file with initial message
+echo -e "PROGRAM OUTPUT:\n" > pOut.txt  #clear the out.txt file with initial message
 
 #availability of my IPv4 address
 mojaIP=$(ifconfig enp0s3 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 #echo "moja ip: $mojaIP"
-ping -c3 $mojaIP >> out.txt 2>&1  #message transfer to file
+ping -c3 $mojaIP >> pOut.txt 2>&1  #message transfer to file
 
 if [ $? -eq 0 ] #if the ping output is 0
 then 
-    echo -e "\nMy IPv4 is avalable." >> IP.txt   #write the connectivity to file
+    echo -e "\nMy IPv4 is available." >> uOUT.txt   #write the connectivity to file
+    echo -e "My IP: $mojaIP" >> uOUT.txt
 else
-    echo -e "\nMy IPv4 is not avalable." >> IP.txt   #write the connectivity to file
+    echo -e "\nMy IPv4 is not available." >> uOUT.txt   #write the connectivity to file
 fi
 
-#for number in $(seq 4 1 16)    #loop, which generates numbers from 1 to 254
-#do
-#    ping -c3 -i3 "$IP"".""$number" >> out.txt 2>&1  #message transfer to file
-#    if [ $? -eq 0 ] #if the ping output is 0
-#    then 
-#        echo -e ""$IP"".""$number"" >> IP.txt   #write the IP to file
-#    fi
-#done
+#availability of my default gateway
+GW=$(/sbin/ip route | awk '/default/ { print $3 }')
+#echo $GW
 
-#ping google 3 times
-ping -c3 -i3 216.58.209.195 >> out.txt 2>&1  #message transfer to file
+ping -c3 $GW >> pOut.txt 2>&1  #message transfer to file
 
 if [ $? -eq 0 ] #if the ping output is 0
 then 
-    echo -e "\nGoogle is available, the internet connection is working." >> IP.txt   #write the connectivity to file
+    echo -e "\nMy default gateway is available." >> uOUT.txt   #write the connectivity to file
+    echo -e "My GateWay: $GW" >> uOUT.txt
 else
-    echo -e "\nGoogle is not available." >> IP.txt   #write the connectivity to file
+    echo -e "\nMy default gateway is not available." >> uOUT.txt   #write the connectivity to file
+fi
+
+#ping google 3 times
+ping -c3 -i3 216.58.209.195 >> pOut.txt 2>&1  #message transfer to file
+
+if [ $? -eq 0 ] #if the ping output is 0
+then 
+    echo -e "\nGoogle is available, the internet connection is working." >> uOUT.txt   #write the connectivity to file
+    echo -e "My IP: 216.58.209.195" >> uOUT.txt
+else
+    echo -e "\nGoogle is not available." >> uOUT.txt   #write the connectivity to file
 fi
 
 #prints out the user output
-echo -e "\n"
-cat IP.txt  #prints the IP's out
+cat uOUT.txt  #prints the IP's out
 
 #duraction of the script
 echo -e "\nDuraction: $SECONDS seconds"
